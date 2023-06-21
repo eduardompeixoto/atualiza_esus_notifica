5f148531d548ea273db155c6d787ed596af4552c
cievs-ses-rj <notifica.ses.rj@gmail.com>
2023-06-21 00:42:16 -0300

    commit 3

---
+++ inst/rodar.R
@@ -0,0 +1,14 @@
+pkgload::load_all()
+pak::pkg_install("WorldHealthOrganization/godataR")
+
+df <- atualiza:::update_godata()
+writexl::write_xlsx(df, "inst/planilha.xlsx")
+# write.csv(df, "inst/tabela_teste.csv")
+
+# escrever a mensagem de commit
+commit_message <-
+  paste0("", Sys.time())
+
+# salvar a mensagem de commit
+writeLines(commit_message, "mensagem-comit.txt")
+
