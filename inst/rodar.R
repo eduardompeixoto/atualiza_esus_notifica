273073e1dbe26db34739a8d498f939b59520ba23
cievs-ses-rj <notifica.ses.rj@gmail.com>
2023-07-28 23:23:19 -0300

    update

--- inst/rodar.R
+++ inst/rodar.R
@@ -1,8 +1,9 @@
 pkgload::load_all()
-pak::pkg_install("WorldHealthOrganization/godataR")
+# pak::pkg_install("WorldHealthOrganization/godataR")
 
-df <- atualiza:::update_godata()
-writexl::write_xlsx(df, "inst/planilha.xlsx")
+# df <- atualiza:::update_godata()
+atualiza:::enviar_alerta()
+# writexl::write_xlsx(df, "inst/planilha.xlsx")
 # write.csv(df, "inst/tabela_teste.csv")
 
 # escrever a mensagem de commit
